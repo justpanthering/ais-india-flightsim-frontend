8149522dc3d2e4f46d81c6cb5618b5a0d76310aa
Ankit Lakra <ankitlakra@outlook.com>
2021-07-03 15:18:03 +0530

    feat: Added loading bar when routing using nprogress, Implement #9

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layout";
 import { Provider } from "next-auth/client";
+import NProgress from "nprogress";
+import router from "next/router";
+import "nprogress/nprogress.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  useEffect(() => {
+    const handleStart = (url: string) =>
+      url !== router.asPath && NProgress.start();
+    const handleComplete = (url: string) =>
+      url === router.asPath && NProgress.done();
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  });
+
   return (
     <Provider session={pageProps.session}>
       <ChakraProvider>
